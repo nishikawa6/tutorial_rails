5ec54467a09949c2c1dd030565e910837a707cc4
nishikawa6 <b053ffv@gmail.com>
2025-08-16 14:39:25 +0900

    docker compose run web rails importmap:installでimportmapを追加し、設定を追加。

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,3 +1,5 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application"
+pin "bootstrap", to: "bootstrap.min.js", preload: true # pinの後ろに書いた文字列で, toで指定したファイルを参照する。
+pin "popper", to: "popper.js", preload: true # import "popper"を指定するとpopper.jsを参照する
