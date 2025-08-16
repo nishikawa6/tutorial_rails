0a853675a8e2fbd3437e1fa9bd3fb5e6b8ba4cac
nishikawa6 <b053ffv@gmail.com>
2025-08-16 14:48:33 +0900

    BootstrapのpopoverやtoolTipの初期化コードを画面読み込み時に処理する設定が必要

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,1 +1,12 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "popper" // importmapで指定した名前を記述
+import "bootstrap"
+
+// 画面読み込み時に呼び出される処理。
+// Bootstrapのpopoverやtooltipはここに手動で初期化コードを記述しなければ動かない。
+window.onload = function () {
+    const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+    const popoverList = [...popoverTriggerList].map(d => new bootstrap.Popover(d))
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    const tooltipList = [...tooltipTriggerList].map(d => new bootstrap.Tooltip(d))
+}
